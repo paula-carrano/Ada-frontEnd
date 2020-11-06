a0a4fe2877d4776f26e8a154991d2219e2a8f8e8
Paula Carrano <paula.agustina.carrano@gmail.com>
2020-11-06 16:08:16 -0300

    finished class 71

--- class71_05nov/script.js
+++ class71_05nov/script.js
@@ -1,90 +1,152 @@
 const inhabitants = getInhabitants();
 
-// 1 Mostrar en pantalla al/los personajes mas enanos de "Brastlewark"
+// 14 Mostrar en pantalla al/los personajes mas enanos de "Brastlewark"
 
 const masBajos = () => {
     let valorMin = inhabitants[0].height
 
-    for (i=0; i < inhabitants.length; i++) {
-        let altura = inhabitants[i].height 
-            if (valorMin > altura) {
-                valorMin = altura
-            }
-        
-       
+    for (i = 0; i < inhabitants.length; i++) {
+        let altura = inhabitants[i].height
+        if (valorMin > altura) {
+            valorMin = altura
+        }
+
+
     }
-//console.log(valorMin)
+    //console.log(valorMin)
 }
 //masBajos();
 
 
-// 2 Crear una funcion que devuelva los personajes por ID en base a un rango numerico.
+// 15 Crear una funcion que devuelva los personajes por ID en base a un rango numerico.
 
-const personajesId = (num1, num2) =>  {
+const personajesId = (num1, num2) => {
     let array = [];
-    for (i=0; i < inhabitants.length; i++) {
+    for (i = 0; i < inhabitants.length; i++) {
         let rangoId = inhabitants[i].id
-        if ( rangoId >= num1 && rangoId <= num2) {
+        if (rangoId >= num1 && rangoId <= num2) {
             array.push(inhabitants[i])
         }
     }
-//return array
+    //return array
 
 }
 //console.table   (personajesId(2,5))
 
 
-// 3 Crear una funcion que devuelva una lista de los habitantes de "Brastlewark" por el color de pelo.
+// 16 Crear una funcion que devuelva una lista de los habitantes de "Brastlewark" por el color de pelo.
 
 const colorPelo = (color) => {
-let pelo = inhabitants.filter((inhabitant) => inhabitant.hair_color == color);
-  
-return pelo;
+    let pelo = inhabitants.filter((inhabitant) => inhabitant.hair_color == color);
+
+    return pelo;
 }
 
 //console.table(colorPelo('Black'))
 
-// 4 Crear una funcion que devuelva una lista de los habitantes de "Brastlewark" en base a una propiedad
+// 17 Crear una funcion que devuelva una lista de los habitantes de "Brastlewark" en base a una propiedad
 // y un valor enviados como parámetros.
 
-const showProperty = (prop, valor) =>{
+const showProperty = (prop, valor) => {
     let property = inhabitants.filter((inhabitant) => inhabitant[prop] == valor);
-  
-return property; 
+
+    return property;
 }
 
 /*console.table(showProperty('age', 306))
 console.table(showProperty('weight', 39.065952))
 console.table(showProperty('professions', "Metalworker"))*/
 
-// 5 Crear una funcion que devuelva al mas alto, al mas bajo, al mas anciano, al mas joven o al mas ancho de "Brastlewark".
+// 18 Crear una funcion que devuelva al mas alto, al mas bajo, al mas anciano, al mas joven o al mas ancho de "Brastlewark".
 
 const habitante = (caracteristica) => {
-    
+    let resultado = {};
+
+    switch (caracteristica) {
+        case 'alto':
+            resultado = getMax('height')
+            return resultado;
+        case 'bajo':
+            resultado = getMin('height')
+            return resultado;
+        case 'anciano':
+            resultado = getMax('age')
+            return resultado;
+        case 'joven':
+            resultado = getMin('age')
+            return resultado;
+        case 'ancho':
+            resultado = getMax('weight')
+            return resultado;
+        default:
+            console.log("No existen valores")
+    }
 }
 
-// 6 Mostrar una tabla con "Fizwood Mysttink" y todos sus amigos.
+const getMax = (prop) =>{
+    let valorMax = inhabitants[0][prop]
+    let inhabitat =  inhabitants[0]
 
-const showFriends = () =>{
+    for (i = 0; i < inhabitants.length; i++) {
+        let value = inhabitants[i][prop]
+        if (value> valorMax) {
+             valorMax = value
+             inhabitat = inhabitants[i]
+        }
+    }
+    return inhabitat
+}
+
+const getMin = (prop) => {
+    let valorMin = inhabitants[0][prop]
+    let inhabitat =  inhabitants[0]
+
+    for (i = 0; i < inhabitants.length; i++) {
+        let value = inhabitants[i][prop]
+        if (valorMin > value) {
+            valorMin = value
+            inhabitat = inhabitants[i]
+        }
+    }
+    return inhabitat
+}
+
+//console.table(habitante('alto'));
+//console.table(habitante('joven'));
+//console.table(habitante('ancho'));
+
+// 19 - Mostrar una tabla con "Fizwood Mysttink" y todos sus amigos.
+
+const showFriends = () => {
+    let fizwood = inhabitants.filter(inhabitant => inhabitant.name === "Fizwood Mysttink");    
+    let friends = fizwood[0].friends;
+    let tabla = "";
+
+    for(i=0; i< friends.length; i++){
+        tabla += `<li> ${friends[i]} </li>`
+    }
+
     
+    document.getElementById("fizwoodFriends").innerHTML = `<h5> Lista de los amigos de ${fizwood[0].name}:</h5> ${tabla}`;
+
 }
 showFriends();
 
-// 7 - Listar a todos los personajes que tengan como profesión "Woodcarver".
+// 20 - Listar a todos los personajes que tengan como profesión "Woodcarver".
 
-const showprofession = (profesion) =>{
+const showprofession = (profesion) => {
     let woodcarver = inhabitants.filter((inhabitant) => inhabitant.professions == profesion);
-return woodcarver; 
+    return woodcarver;
 }
 
 //console.table(showprofession("Woodcarver"));
 
-// 8 - Listar a todos los personajes que tengan como profesion un dato enviado como como parámetro.
+// 21 - Listar a todos los personajes que tengan como profesion un dato enviado como como parámetro.
 
-const showprofession2 = (profesion) =>{
-    let profession= inhabitants.filter((inhabitant) => inhabitant.professions == profesion);
-return profession; 
+const showprofession2 = (profesion) => {
+    let profession = inhabitants.filter((inhabitant) => inhabitant.professions == profesion);
+    return profession;
 }
 
-console.table(showprofession("Carpenter"));
+//console.table(showprofession("Carpenter"));
 
